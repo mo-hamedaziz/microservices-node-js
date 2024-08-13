3029057159f38ad045ced64e5af70164529a3ddf
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-08-13 17:40:07 +0100

    Solve database connection error

--- auth-service/config/database.js
+++ auth-service/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+const sequelize = new Sequelize(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOSTNAME}:${process.env.POSTGRES_PORT}/${process.env.AUTH_DB_NAME}`, {
   dialect: 'postgres',
   logging: false,
 });
--- auth-service/index.js
+++ auth-service/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 require("express-async-errors");
+require('dotenv').config()
+
 const sequelize = require("./config/database");
 const authRoutes = require("./routes/authRoutes");
 
@@ -8,7 +10,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Authentication service is up and running at ${PORT}`);
 });
 
