537139396183c7baef45a924de0cdef5b0200bb0
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-08-16 19:27:58 +0100

    Hide db connection params

--- auth-service/config/database.js
+++ auth-service/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = `mongodb://user:pass@auth-db:27017`;
+const mongoUri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
 
 mongoose.connect(mongoUri)
   .then(() => console.log('Connection to MongoDB has been established successfully.'))
