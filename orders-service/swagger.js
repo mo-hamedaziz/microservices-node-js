b1333756c31fec9ebca3f1f3865ebec6df5e4b42
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-08-18 15:32:37 +0100

    Service app and running

--- orders-service/swagger.js
+++ orders-service/swagger.js
@@ -12,7 +12,7 @@ const swaggerDefinition = {
   servers: [
     {
       url: 'http://localhost:3003',
-      description: 'Development server',
+      description: 'Local server',
     },
   ],
 };
