858eb75fb18fd5234593d545dd92aaf137bd97cd
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2024-08-20 00:49:56 +0100

    Configure docker compose auth service

--- auth-service/swagger.js
+++ auth-service/swagger.js
@@ -10,7 +10,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: `http://localhost:3001`,
+      url: `http://localhost:${process.env.PORT}`,
       description: 'Local server',
     },
   ],
